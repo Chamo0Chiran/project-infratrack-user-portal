ee1a58ad07f8689b47585e4f9d87e27a64755e89
dushydi <dulshanya.20230243@iit.ac.lk>
2025-02-17 13:57:29 +0530

    User implementation

--- infratrack_userportal-backend/src/main/java/com/G153/InfratrackUserPortal/InfratrackUserPortalApplication.java
+++ infratrack_userportal-backend/src/main/java/com/G153/InfratrackUserPortal/InfratrackUserPortalApplication.java
@@ -1,6 +1,5 @@
 package com.G153.InfratrackUserPortal;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InfratrackUserPortalApplication {
 
 	public static void main(String[] args) {
-		// Load .env file
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-
-		// Set environment variables for Spring Boot
-		System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
 		SpringApplication.run(InfratrackUserPortalApplication.class, args);
 	}
 
